cea437aafabdbe97cfd213d32e348d8c7cd67f4b
Lev Sivashov <lev@yoxel.com>
2017-02-24 01:35:49 +0200

    chapter 9 - algebra for parsers - few laws

--- src/com/github/lpld/study/funcscala/chapter9/Parser.scala
+++ src/com/github/lpld/study/funcscala/chapter9/Parser.scala
@@ -1,5 +1,8 @@
 package com.github.lpld.study.funcscala.chapter9
 
+import com.github.lpld.study.funcscala.chapter8.Gen.forAll
+import com.github.lpld.study.funcscala.chapter8.{Gen, Prop}
+
 import scala.language.implicitConversions
 
 /**
@@ -11,7 +14,9 @@ object Parser {
 
 trait Parsers[ParseError, Parser[+ _]] { self =>
 
-  def char(c: Char): Parser[Char]
+  def char(c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
+
+  def succeed[A](a: A): Parser[A] = string("") map (_ => a)
 
   def orString(s1: String, s2: String): Parser[String]
 
@@ -35,5 +40,16 @@ trait Parsers[ParseError, Parser[+ _]] { self =>
     def many: Parser[List[A]] = ???
 
     def map[B](f: A => B): Parser[B] = ???
+  }
+
+  object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, p map identity)(in)
+
+    def unitLaw[A](as: Gen[A], ss: Gen[String]): Prop =
+      forAll(as.map2(ss)((_, _))) { case (a, s) => run(succeed(a))(s) == Right(a) }
   }
 }
